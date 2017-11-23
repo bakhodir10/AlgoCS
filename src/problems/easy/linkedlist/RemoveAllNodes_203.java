ce2a3259a9fff54a1e3bb1355c0dfcf8650d134a
bakhodirsbox <bahodir9293@gmail.com>
2017-11-23 17:00:22 -0600

    solved Remove All Nodes problem

--- src/problems/easy/linkedlist/RemoveAllNodes_203.java
+++ src/problems/easy/linkedlist/RemoveAllNodes_203.java
@@ -1,55 +1,18 @@
 package problems.easy.linkedlist;
 
-// todo not done
 public class RemoveAllNodes_203 {
 
-    public ListNode removeElements(ListNode head, int val) {
+    public ListNode remove(ListNode head, int val) {
         ListNode temp = new ListNode(Integer.MIN_VALUE);
         temp.next = head;
         ListNode dummy = temp;
         while (temp.next != null) {
             if (temp.next.val == val) {
                 temp.next = temp.next.next;
-                head = dummy.next;
-            }
-            temp = temp.next;
+            } else temp = temp.next;
         }
+        head = dummy.next;
         return head;
-    }
-
-    public void add() {
-        ListNode node = new ListNode(6);
-        ListNode node1 = new ListNode(-16);
-        ListNode node2 = new ListNode(3);
-        ListNode node3 = new ListNode(0);
-        ListNode head = new ListNode(94);
-        head.next = node;
-        head.val = node.val;
-        head = node;
-//        head.next = node1;
-//        head.val = node1.val;
-//        head = node1;
-//        head.next = node2;
-//        head.val = node2.val;
-//        head = node2;
-//        head.next = node3;
-//        head.val = node3.val;
-//        head = node3;
-//        delete(head, 6);
-        iterate(head);
-    }
-
-    public void iterate(ListNode head) {
-        ListNode temp = head;
-        while (temp.next != null) {
-            System.out.println(temp.val);
-            temp = temp.next;
-        }
-    }
-
-    public static void main(String[] args) {
-        RemoveAllNodes_203 list = new RemoveAllNodes_203();
-        list.add();
     }
 
     private class ListNode {

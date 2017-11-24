688a6f8f4e0ce520037aea1276cd503c309088de
bakhodirsbox <bahodir9293@gmail.com>
2017-11-24 01:09:22 -0600

    solved Remove duplicate elements

--- src/problems/easy/linkedlist/ReverseNodes_206.java
+++ src/problems/easy/linkedlist/ReverseNodes_206.java
@@ -2,19 +2,51 @@ package problems.easy.linkedlist;
 
 // todo not done
 public class ReverseNodes_206 {
-    public static void main(String[] args) {
 
+    public ListNode reverse(ListNode head) {
+
+        return null;
+
+        /*
+            3 0 9 7 2
+            2 0 9 7 3
+            2 7 9 0 3
+         */
     }
 
-    public void reverse(ListNode head) {
+    public void add() {
+        ListNode head = new ListNode(5);
+        ListNode node = new ListNode(5);
+        ListNode node1 = new ListNode(5);
+        ListNode node2 = new ListNode(9);
+        ListNode temp = head;
+        head.next = node;
+        head = head.next;
+        head.next = node1;
+        head = head.next;
+        head.next = node2;
+        head = temp;
+        reverse(head);
+    }
 
+    public void iterate(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseNodes_206 list = new ReverseNodes_206();
+        list.add();
     }
 
     private class ListNode {
         int val;
         ListNode next;
 
-        ListNode(int val) {
+        public ListNode(int val) {
             this.val = val;
         }
     }

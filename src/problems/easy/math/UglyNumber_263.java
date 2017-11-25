e82413dc1308a2ce19ff85b876ba17f602bdb8ae
bakhodirsbox <bahodir9293@gmail.com>
2017-11-25 10:26:48 -0600

    Sum of Square numbers problems is solved

--- src/problems/easy/math/UglyNumber_263.java
+++ src/problems/easy/math/UglyNumber_263.java
@@ -2,11 +2,6 @@ package problems.easy.math;
 
 // todo not done
 public class UglyNumber_263 {
-
-    public static void main(String[] args) {
-
-    }
-
     public boolean isUgly(int num){
         return true;
     }

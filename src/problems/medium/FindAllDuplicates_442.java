d582c1077be890cd5e444166b6923d0e5dac6293
bakhodirsbox <bahodir9293@gmail.com>
2017-12-11 18:08:05 -0600

    solved Find All Duplicates problem

--- src/problems/medium/FindAllDuplicates_442.java
+++ src/problems/medium/FindAllDuplicates_442.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FindAllDuplicates_442 {
-    public List<Integer> findDuplicates(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) res.add(Math.abs(index + 1));
+            nums[index] = -nums[index];
         }
-        return list;
+        return res;
     }
 }

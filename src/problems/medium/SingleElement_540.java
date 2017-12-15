a1d75b1bf9172931bf7f0b74e7317e4376fcb1cd
bakhodirsbox <bahodir9293@gmail.com>
2017-12-15 01:07:14 -0600

    Single Element problem has been solved

--- src/problems/medium/SingleElement_540.java
+++ src/problems/medium/SingleElement_540.java
@@ -1,6 +1,6 @@
 package problems.medium;
 
-public class Single_Element_540 {
+public class SingleElement_540 {
     public static int singleNonDuplicate(int nums[]) {
         int single = 0;
         for (int i = 0; i < nums.length; i++) single ^= nums[i];

dce431cf5c6b086bdc16ca6a738ba8cfe8da50c9
bakhodirsbox <bahodir9293@gmail.com>
2017-12-21 00:18:56 -0600

    problem solved Find smallest letter

---
+++ src/problems/easy/FindSmallestLetter_744.java
@@ -0,0 +1,10 @@
+package problems.easy;
+
+public class FindSmallestLetter_744 {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        for (int i = 0; i < letters.length; i++) {
+            if (letters[i] > target) return letters[i];
+        }
+        return letters[0];
+    }
+}

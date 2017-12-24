9ce418b27e8b7c6c7c7592b19f8c86418ec726cf
bakhodirsbox <bahodir9293@gmail.com>
2017-12-24 17:33:00 -0600

    Find the difference between two string problem solved

--- src/problems/easy/FindTheDifference_389.java
+++ src/problems/easy/FindTheDifference_389.java
@@ -1,42 +1,14 @@
 package problems.easy;
 
-// todo not done
 public class FindTheDifference_389 {
-    public static void main(String[] args) {
-        System.out.println(findTheDifference("abcd", "abcde"));
-//        System.out.println(findTheDifference(
-//                "ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwl\n" +
-//                        "hvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnis\n" +
-//                        "chrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnh\n" +
-//                        "fzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddm\n" +
-//                        "wlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyj\n" +
-//                        "xswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwu\n" +
-//                        "kprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderl\n" +
-//                        "qarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutv\n" +
-//                        "jchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwg\n" +
-//                        "vpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor",
-//                "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdy\n" +
-//                        "bfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlite\n" +
-//                        "grlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwl\n" +
-//                        "rxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyj\n" +
-//                        "oobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhc\n" +
-//                        "wzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcv\n" +
-//                        "vjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfet\n" +
-//                        "cdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgro\n" +
-//                        "rudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiy\n" +
-//                        "ldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"));
-    }
-
     public static char findTheDifference(String s, String t) {
-        for (int i = 0; i < s.length(); i++) {
-            String temp = t.substring(0, i) + t.substring(i + 1, t.length());
-            int count1 = 0;
-            int count2 = 0;
-            for (int j = 0; j < temp.length(); j++) {
-                count1 ^= temp.charAt(i);
-                count2 ^= temp.charAt(i);
-            }
-            if (count1 == count2) return t.charAt(i);
+        char a[] = new char[26], b[] = new char[26];
+        for (int i = 0; i < t.length(); i++) {
+            if (i < s.length()) a[s.charAt(i) - 'a']++;
+            b[t.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return (char) (i + 'a');
         }
         return '.';
     }

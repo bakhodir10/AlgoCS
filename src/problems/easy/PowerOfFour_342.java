369004c812d8284a243578fddc17445f81abe39a
bakhodirsbox <bahodir9293@gmail.com>
2017-12-27 18:11:05 -0600

    Power of Four problem has been solved

---
+++ src/problems/easy/PowerOfFour_342.java
@@ -0,0 +1,9 @@
+package problems.easy;
+
+public class PowerOfFour_342 {
+    public static boolean isPowerOfFour(int num) {
+        if (num < 0) return false;
+        String bin = Integer.toBinaryString(num);
+        return Integer.bitCount(num) == 1 && (bin.length() - 1) % 2 == 0;
+    }
+}

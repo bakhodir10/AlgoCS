173a05eab245f597336c5d271e17f989ec0e5116
bakhodirsbox <bahodir9293@gmail.com>
2017-12-29 11:05:37 -0600

    Binary Tree Level Order problem has been solved

---
+++ src/problems/medium/BinTreeLevelOrder_102.java
@@ -0,0 +1,37 @@
+package problems.medium;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinTreeLevelOrder_102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> list = new LinkedList<>();
+        if (root == null) return list;
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> l = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode t = q.poll();
+                l.add(t.val);
+                if (t.left != null) q.offer(t.left);
+                if (t.right != null) q.offer(t.right);
+            }
+            list.add(l);
+            l = new LinkedList<>();
+        }
+        return list;
+    }
+
+    private class TreeNode {
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}

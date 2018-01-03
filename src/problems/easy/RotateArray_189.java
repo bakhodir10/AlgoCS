53f34e4a05da0371dbafe78bdc9450e40282c3c0
bakhodirsbox <bahodir9293@gmail.com>
2018-01-03 02:06:51 -0600

    Rotate array solved leetcode problem

--- src/problems/easy/RotateArray_189.java
+++ src/problems/easy/RotateArray_189.java
@@ -1,16 +1,34 @@
 package problems.easy;
 
-// todo not done
 public class RotateArray_189 {
-    public static void main(String[] args) {
-        rotate1(new int[]{2, 1}, 1);
+
+    // approach 1
+    public void rotate1(int[] arr, int k) {
+        k %= arr.length;
+        reverse(arr, 0, arr.length);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, arr.length);
     }
 
-    public static void rotate1(int[] arr, int k) {
-        for (int i = 0; i < arr.length; i++) {
-            int temp = arr[i];
-            arr[i] = arr[k % arr.length + i];
+    private void reverse(int[] arr, int from, int to) {
+        while (from < to) {
+            int temp = arr[from];
+            arr[from] = arr[to];
+            arr[to] = temp;
+            from++;
+            to--;
+        }
+    }
 
+    // approach 2
+    public static void rotate2(int[] arr, int k) {
+        for (int i = 0; i < k % arr.length; i++) {
+            int temp = arr[arr.length - 1];
+            for (int j = 0; j < arr.length; j++) {
+                int c = arr[j];
+                arr[j] = temp;
+                temp = c;
+            }
         }
     }
 }

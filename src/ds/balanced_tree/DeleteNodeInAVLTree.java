2d9691fa014d78709dbf53f7012568c9c288d409
bakhodirsbox <bahodir9293@gmail.com>
2018-01-04 09:57:05 -0600

    AVL tree implementation

--- src/ds/balanced_tree/DeleteNodeInAVLTree.java
+++ src/ds/balanced_tree/DeleteNodeInAVLTree.java
@@ -1,80 +1,19 @@
 package ds.balanced_tree;
 
 @SuppressWarnings("Duplicates")
-public class AVLTree<E extends Comparable<E>> {
+public class DeleteNodeInAVLTree<E extends Comparable<E>> {
     private AVLNode<E> root;
 
-    private int height(AVLNode<E> n) {
-        return n == null ? -1 : n.height;
+    private int height(AVLNode<E> node) {
+        return node == null ? -1 : node.height;
     }
 
     private int max(int a, int b) {
         return a > b ? a : b;
     }
 
-    private int getBalance(AVLNode<E> n) {
-        return n == null ? 0 : height(n.left) - height(n.right);
-    }
-
-    private AVLNode<E> leftRotate(AVLNode<E> x) {
-        AVLNode<E> y = x.right;
-        AVLNode<E> z = y.left;
-
-        // rotation
-        y.left = x;
-        x.right = z;
-
-        y.height = max(height(y.left), height(y.right)) + 1;
-        x.height = max(height(x.left), height(x.right)) + 1;
-        return y;
-    }
-
-    private AVLNode<E> rightRotate(AVLNode<E> y) {
-        AVLNode<E> x = y.left;
-        AVLNode<E> z = x.right;
-
-        //rotation
-        x.right = y;
-        y.left = z;
-
-        x.height = max(height(x.left), height(x.right)) + 1;
-        y.height = max(height(y.left), height(y.right)) + 1;
-        return x;
-    }
-
-    public void insert(E elem) {
-        if (elem == null) throw new IllegalArgumentException();
-        root = insertHelper(root, elem);
-    }
-
-    private AVLNode<E> insertHelper(AVLNode<E> n, E elem) {
-        // usual insert
-        if (n == null) return new AVLNode<>(elem);
-        if (n.elem.compareTo(elem) > 0) n.left = insertHelper(n.left, elem);
-        else if (n.elem.compareTo(elem) < 0) n.right = insertHelper(n.right, elem);
-
-        // increase height of the node
-        n.height = 1 + max(height(n.left), height(n.right));
-
-        // get the balance of current node
-        int balance = getBalance(n);
-
-        // if it is not balanced, there are 4 types.
-        // left-left case
-        if (balance > 1 && n.elem.compareTo(elem) > 0) return rightRotate(n);
-            // right-right case
-        else if (balance < -1 && n.elem.compareTo(elem) < 0) return leftRotate(n);
-            //left-right case
-        else if (balance > 1 && n.left.elem.compareTo(elem) < 0) {
-            n.left = leftRotate(n.left);
-            return rightRotate(n);
-        }
-        //right-left case
-        else if (balance < -1 && n.right.elem.compareTo(elem) > 0) {
-            n.right = rightRotate(n.right);
-            return leftRotate(n);
-        }
-        return n;
+    private int getBalance(AVLNode<E> node) {
+        return node == null ? 0 : height(node.left) - height(node.right);
     }
 
     public void delete(E elem) {
@@ -127,6 +66,34 @@ public class AVLTree<E extends Comparable<E>> {
             }
         }
         return node;
+    }
+
+    private AVLNode<E> leftRotate(AVLNode<E> x) {
+        AVLNode<E> y = x.right;
+        AVLNode<E> z = y.left;
+
+        // rotation
+        y.left = x;
+        x.right = z;
+
+        // update heights
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+        return x;
+    }
+
+    private AVLNode<E> rightRotate(AVLNode<E> y) {
+        AVLNode<E> x = y.left;
+        AVLNode<E> z = x.right;
+
+        //rotation
+        x.right = y;
+        y.left = z;
+
+        // update heights
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+        return x;
     }
 
     private class AVLNode<E> {
--- src/ds/balanced_tree/InsertNodeInAVLTree.java
+++ src/ds/balanced_tree/InsertNodeInAVLTree.java
@@ -1,7 +1,7 @@
 package ds.balanced_tree;
 
 @SuppressWarnings("Duplicates")
-public class AVLTree<E extends Comparable<E>> {
+public class InsertNodeInAVLTree<E extends Comparable<E>> {
     private AVLNode<E> root;
 
     private int height(AVLNode<E> n) {
@@ -24,6 +24,7 @@ public class AVLTree<E extends Comparable<E>> {
         y.left = x;
         x.right = z;
 
+        // update heights
         y.height = max(height(y.left), height(y.right)) + 1;
         x.height = max(height(x.left), height(x.right)) + 1;
         return y;
@@ -37,6 +38,7 @@ public class AVLTree<E extends Comparable<E>> {
         x.right = y;
         y.left = z;
 
+        // update heights
         x.height = max(height(x.left), height(x.right)) + 1;
         y.height = max(height(y.left), height(y.right)) + 1;
         return x;
@@ -75,58 +77,6 @@ public class AVLTree<E extends Comparable<E>> {
             return leftRotate(n);
         }
         return n;
-    }
-
-    public void delete(E elem) {
-        if (elem == null || root == null) throw new IllegalArgumentException();
-        root = deleteHelper(root, elem);
-    }
-
-    private AVLNode<E> deleteHelper(AVLNode<E> node, E elem) {
-        // usual delete node from tree
-        if (node.elem.compareTo(elem) > 0) node.left = deleteHelper(node.left, elem);
-        if (node.elem.compareTo(elem) < 0) node.right = deleteHelper(node.right, elem);
-        else {
-            if (node.left == null && node.right == null) {        // case 1: the node has no child
-                node = null;
-            } else if (node.left == null || node.right == null) { // case 2: the node has a child
-                if (node.left != null) node = node.left;
-                else node = node.right;
-            } else {                                              // case 3: the node has two child
-                AVLNode<E> p = node.left;
-                if (p.right != null) {
-                    while (p.right.right != null) p = p.right;
-                    node.elem = p.right.elem;
-                    p.right = null;
-                } else {
-                    p.right = node.right;
-                    node = p;
-                }
-                node.height = max(height(node.left), height(node.right)) + 1;
-                int balance = getBalance(node);
-
-                // if the node is not balanced
-                // left-left case
-                if (balance > -1 && getBalance(node.left) >= 0) {
-                    return rightRotate(node);
-                }
-                // left-right case
-                if (balance > -1 && getBalance(node.left) < 0) {
-                    node.left = leftRotate(node);
-                    return rightRotate(node);
-                }
-                // right-right case
-                if (balance < -1 && getBalance(node.right) <= 0) {
-                    return leftRotate(node);
-                }
-                // right-left case
-                if (balance < -1 && getBalance(node.right) > -1) {
-                    node.right = rightRotate(node);
-                    return leftRotate(node);
-                }
-            }
-        }
-        return node;
     }
 
     private class AVLNode<E> {

4a017b5f0524983fbd24a14c2ab72f8de9c68e50
bakhodirsbox <bahodir9293@gmail.com>
2018-01-11 17:14:19 -0600

    Tree traversal in iterative way

--- src/ds/tree/TraversalIterative.java
+++ src/ds/tree/TraversalIterative.java
@@ -1,15 +1,32 @@
-package problems.medium;
+package ds.tree;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
-/*
-Note: Recursive solution is trivial, could you do it iteratively?
- */
-
 @SuppressWarnings("Duplicates")
-public class BinTreeInOrder_94 {
+public class TraversalIterative {
+
+    // preorder traversal
+    public List<Integer> preorderTraversal(TreeNode root) {
+        TreeNode node = root;
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        while (node != null && !st.isEmpty()) {
+            while (node != null) {
+                st.push(node);
+                list.add(node.val);
+                node = node.left;
+            }
+            if (!st.isEmpty()) {
+                TreeNode popped = st.pop();
+                if (popped.right != null) node = popped.right;
+            }
+        }
+        return list;
+    }
+
+    // inorder traversal
     public List<Integer> inorderTraversal(TreeNode root) {
         if (root == null) return new LinkedList<>();
         List<Integer> list = new LinkedList<>();
@@ -31,14 +48,12 @@ public class BinTreeInOrder_94 {
         return list;
     }
 
-
-    public class TreeNode {
+    private class TreeNode {
         int val;
-        TreeNode left;
-        TreeNode right;
+        TreeNode left, right;
 
-        TreeNode(int x) {
-            val = x;
+        public TreeNode(int val) {
+            this.val = val;
         }
     }
 }
---
+++ src/problems/medium/BinTreePreorder_144.java
@@ -0,0 +1,39 @@
+package problems.medium;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+Note: Recursive solution is trivial, could you do it iteratively?
+ */
+
+@SuppressWarnings("Duplicates")
+public class BinTreePreorder_144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        TreeNode node = root;
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> st = new Stack<>();
+        while (node != null && !st.isEmpty()) {
+            while (node != null) {
+                st.push(node);
+                list.add(node.val);
+                node = node.left;
+            }
+            if (!st.isEmpty()) {
+                TreeNode popped = st.pop();
+                if (popped.right != null) node = popped.right;
+            }
+        }
+        return list;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}

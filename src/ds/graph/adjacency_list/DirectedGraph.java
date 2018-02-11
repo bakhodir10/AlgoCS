394ec88b081783859d3f1e420b10345709b05697
bakhodirsbox <bahodir9293@gmail.com>
2018-02-11 12:02:51 -0600

    Directed Graph implementation

--- src/ds/graph/adjacency_list/DirectedGraph.java
+++ src/ds/graph/adjacency_list/DirectedGraph.java
@@ -16,30 +16,39 @@ public class DirectedGraph<V> {
         this.vertices = new HashMap<>();
     }
 
-    // add a new edge to the vertex/node
+    // add vertex/node with set of edges
     public void addEdge(V vertex, Set<V> edges) {
+        if (vertex == null) throw new NullPointerException("Vertex cannot be null");
         if (this.vertices.containsKey(vertex)) this.vertices.get(vertex).addAll(edges);
         else this.vertices.put(vertex, edges);
     }
 
     // get edges of the vertex/node
-    public Set<V> getEdges(V elem) {
-        return this.vertices.get(elem);
+    public Set<V> getEdges(V vertex) {
+        boolean exist = this.vertices.containsKey(vertex);
+        if (!exist) throw new NullPointerException("There is no vertex regarding to given input");
+        return this.vertices.get(vertex);
     }
 
     // remove the given a vertex/node
-    public void deleteVertex(V elem) {
-        this.vertices.remove(elem);
+    public boolean deleteVertex(V vertex) {
+        Set<V> set = this.vertices.get(vertex);
+        if (set == null) throw new NullPointerException("There is no vertex regarding to given input");
+        return this.vertices.remove(vertex) != null;
     }
 
     // remove all edges of the vertex/node
-    public void deleteAllEdges(V elem) {
-        if (this.vertices.containsKey(elem)) this.vertices.put(elem, new HashSet<>());
+    public boolean deleteAllEdges(V vertex) {
+        if (this.vertices.containsKey(vertex)) {
+            this.vertices.put(vertex, new HashSet<>());
+            return true;
+        } else throw new NullPointerException("There is no vertex regarding to given input");
     }
 
     // remove all vertices/nodes
-    public void deleteAllVertices() {
+    public boolean deleteAllVertices() {
         this.vertices = new HashMap<>();
+        return true;
     }
 
     // print all vertices with its edges

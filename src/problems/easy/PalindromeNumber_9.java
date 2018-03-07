757654b8fcf3dc65947f7c0088cadb65bccd79e0
bakhodirsbox <bahodir9293@gmail.com>
2018-03-07 17:28:38 -0600

    solved Palindrome number on leetcode

--- src/problems/easy/PalindromeNumber_9.java
+++ src/problems/easy/PalindromeNumber_9.java
@@ -1,11 +1,10 @@
 package problems.easy;
 
-public class PalindromeNumber {
-    public static boolean isPalindrome(int number){
+public class PalindromeNumber_9 {
+    public static boolean isPalindrome(int number) {
         if (number < 0) return false;
-        int temp = number;
-        long rev = 0;
-        while (number != 0){
+        int temp = number, rev = 0;
+        while (number != 0) {
             rev = rev * 10 + number % 10;
             number /= 10;
         }

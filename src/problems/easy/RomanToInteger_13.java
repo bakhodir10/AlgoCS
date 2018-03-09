404829e2b453961c3b990d3f5207d0d271597493
bakhodirsbox <bahodir9293@gmail.com>
2018-03-09 14:57:23 -0600

    Roman to integer solved on leetcode

--- src/problems/easy/RomanToInteger_13.java
+++ src/problems/easy/RomanToInteger_13.java
@@ -8,44 +8,40 @@ public class RomanToInteger_13 {
         char last = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'I') {
+            char current = s.charAt(i);
+
+            if (current == 'I') {
                 last = 'I';
                 res += 1;
-            } else if (s.charAt(i) == 'V') {
-                if (last == 'I') {
-                    res += 3;
-                } else res += 5;
+            }
+            if (current == 'V') {
+                if (last == 'I') res += 3;
+                else res += 5;
                 last = 'V';
             }
-            if (s.charAt(i) == 'X') {
-                if (last == 'I') {
-                    res += 8;
-                } else res += 10;
+            if (current == 'X') {
+                if (last == 'I') res += 8;
+                else res += 10;
                 last = 'X';
             }
-
-            if (s.charAt(i) == 'L') {
-                if (last == 'X') {
-                    res += 30;
-                } else res += 50;
+            if (current == 'L') {
+                if (last == 'X') res += 30;
+                else res += 50;
                 last = 'L';
             }
-            if (s.charAt(i) == 'C') {
-                if (last == 'X') {
-                    res += 80;
-                } else res += 100;
+            if (current == 'C') {
+                if (last == 'X') res += 80;
+                else res += 100;
                 last = 'C';
             }
-            if (s.charAt(i) == 'D') {
-                if (last == 'C') {
-                    res += 300;
-                } else res += 500;
+            if (current == 'D') {
+                if (last == 'C') res += 300;
+                else res += 500;
                 last = 'D';
             }
-            if (s.charAt(i) == 'M') {
-                if (last == 'C') {
-                    res += 800;
-                } else res += 1000;
+            if (current == 'M') {
+                if (last == 'C') res += 800;
+                else res += 1000;
                 last = 'M';
             }
         }

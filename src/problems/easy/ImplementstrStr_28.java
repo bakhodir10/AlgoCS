e8ba14717f8e93d7c459b81f0b21414006e61a08
bakhodirsbox <bahodir9293@gmail.com>
2018-04-07 09:36:18 -0500

    Implement Str problem solved on leetcode

---
+++ src/problems/easy/ImplementstrStr_28.java
@@ -0,0 +1,7 @@
+package problems.easy;
+
+public class ImplementstrStr_28 {
+    public int strStr(String haystack, String needle) {
+        return haystack == null || needle == null ? -1 : haystack.indexOf(needle);
+    }
+}

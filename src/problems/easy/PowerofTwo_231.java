8cb7b7358b4612e0c5991fae2b5c25617d8accd3
Bakhodir <bakhodir9293@gmail.com>
2018-08-03 16:11:46 -0500

    Some problem from leetcode

--- src/problems/easy/PowerofTwo_231.java
+++ src/problems/easy/PowerofTwo_231.java
@@ -9,11 +9,31 @@ public class PowerofTwo_231 {
 
     // approach 2
     public boolean isPowerOfTwo2(int n) {
-        long sum = 1, i = 0;
+        long sum = 1;
         while (sum < n) {
             sum *= 2;
-            i++;
         }
         return sum == n;
+    }
+
+    // approach 3
+    public boolean isPowerOfTwo3(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        if (n % 2 != 0) return false;
+        return isPowerOfTwo3(n / 2);
+    }
+
+    // approach 4
+    public boolean isPowerOfTwo4(int n) {
+        return n > 0 && isPowerOfTwoHelper(n) == n;
+    }
+
+    private int isPowerOfTwoHelper(int n) {
+        if (n == 1) return 1;
+        else {
+            int prev = isPowerOfTwoHelper(n / 2);
+            return prev * 2;
+        }
     }
 }

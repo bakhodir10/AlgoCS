98d0a8dba282f9852db4244bbaa735b70ecdff5e
Bakhodir <bakhodir9293@gmail.com>
2018-08-22 15:40:49 -0500

    Delete duplicates problem

---
+++ src/interviews/DeleteDupsArray.java
@@ -0,0 +1,33 @@
+package interviews;
+
+/*
+    Delete all the duplicates in given unsorted array. Delete means moving an element to the end of the array.
+    Can you do it in-place and without using any additional memory ?
+    Input: 2 6 2 5 2 3 7 5
+    Output: 2 6 5 3 7
+ */
+
+import java.util.Arrays;
+
+public class DeleteDupsArray {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(deleteDups(new int[]{2, 2, 5, 2, 4, 6, 2, 7, 2})));
+    }
+
+    // Approach 1. Time Complexity: O(n^2). Space complexity: O(1)
+    public static int[] deleteDups(int nums[]) {
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] != nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    break;
+                }
+            }
+        }
+        return nums;
+    }
+}

e59316859fded2f7ded48e1dec043fcd1868ffeb
bakhodir10 <bahodir9293@gmail.com>
2020-06-28 18:11:20 -0600

    leetcode explorer topic: binary tree

---
+++ src/leetcode_explore/trees/recurcive/PreOrderTraversal.java
@@ -0,0 +1,24 @@
+package leetcode_explore.trees.recurcive;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PreOrderTraversal {
+
+    private List<Integer> preOrderedList = new LinkedList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root != null) {
+            preOrderedList.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return preOrderedList;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+    }
+}

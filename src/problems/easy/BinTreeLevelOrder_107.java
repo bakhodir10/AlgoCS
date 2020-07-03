c4435f54246c5d1ddf66867a330173efd7aabd0a
bakhodir10 <bahodir9293@gmail.com>
2020-07-02 18:39:35 -0600

    binary tree level order solved on leetcode

--- src/problems/easy/BinTreeLevelOrder_107.java
+++ src/problems/easy/BinTreeLevelOrder_107.java
@@ -1,33 +1,50 @@
 package problems.easy;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Stack;
 
+@SuppressWarnings("Duplicates")
 public class BinTreeLevelOrder_107 {
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+    // approach 1. BFS
+    public List<List<Integer>> levelOrderBottom1(TreeNode root) {
         if (root == null) return new LinkedList<>();
-        List<List<Integer>> list = new LinkedList<>();
-        List<Integer> l = new LinkedList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        Stack<List<Integer>> st = new Stack<>();
-        q.offer(root);
-        while (!q.isEmpty()) {
-            int size = q.size();
+        LinkedList<List<Integer>> allLevelValues = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> levelValues = new LinkedList<>();
             for (int i = 0; i < size; i++) {
-                TreeNode t = q.poll();
-                l.add(t.val);
-                if (t.left != null) q.offer(t.left);
-                if (t.right != null) q.offer(t.right);
+                TreeNode t = queue.poll();
+                levelValues.add(t.val);
+                if (t.left != null) queue.offer(t.left);
+                if (t.right != null) queue.offer(t.right);
             }
-            st.push(l);
-            l = new LinkedList<>();
+            allLevelValues.addFirst(levelValues);
         }
-        while (!st.isEmpty()) {
-            list.add(st.pop());
-        }
-        return list;
+        return allLevelValues;
+    }
+
+    // approach 2. DFS
+    LinkedList<List<Integer>> allLevelValues = new LinkedList<>();
+
+    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+        if (root == null) return new LinkedList<>();
+        helper(root, 0);
+        Collections.reverse(allLevelValues);
+        return allLevelValues;
+    }
+
+    private void helper(TreeNode root, int level) {
+        if (root == null) return;
+        if (level == allLevelValues.size()) allLevelValues.add(new LinkedList<>());
+        allLevelValues.get(level).add(root.val);
+        helper(root.left, level + 1);
+        helper(root.right, level + 1);
     }
 
     private class TreeNode {

ba22fdad2c0b90428b18007b0cef19739565c7a8
bakhodir10 <bahodir9293@gmail.com>
2020-08-20 02:16:49 -0600

    minimum window substring code optimization by using array

--- src/problems/hard/MinimumWindowSubstring_76.java
+++ src/problems/hard/MinimumWindowSubstring_76.java
@@ -1,45 +1,29 @@
 package problems.hard;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MinimumWindowSubstring_76 {
 
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) return "";
-        Map<Character, Integer> charsWithCount = new HashMap<>();
-        int minLen = 0, slow = 0, start = 0, end = 0;
 
-        for (char c : t.toCharArray()) charsWithCount.put(c, charsWithCount.getOrDefault(c, 0) + 1);
-        int currentDiffCharsCount = charsWithCount.size();
+        int min = Integer.MAX_VALUE, startIndex = 0, endIndex = 0, slow = 0;
+        int counter = t.length();
+        int[] arr = new int[128];
 
-        for (int fast = 0; fast < s.length(); fast ++) {
-            if (!charsWithCount.containsKey(s.charAt(fast))) continue;
+        for (char c : t.toCharArray()) arr[c]++;
 
-            int charCountFast = charsWithCount.get(s.charAt(fast));
-            charsWithCount.put(s.charAt(fast), --charCountFast);
-            if (charCountFast == 0) currentDiffCharsCount--;
-
-            while (currentDiffCharsCount == 0) {
-
-                if (minLen == 0) {
-                    minLen = fast - slow + 1;
-                    start = slow;
-                    end = fast + 1;
-                } else if (minLen > fast - slow + 1) {
-                    minLen = fast - slow + 1;
-                    start = slow;
-                    end = fast + 1;
+        for (int fast = 0; fast < s.length(); fast++) {
+            if (arr[s.charAt(fast)]-- > 0) counter--;
+            while (counter == 0) {
+                if (fast - slow < min) {
+                    startIndex = slow;
+                    endIndex = fast;
+                    min = fast - slow;
                 }
+                arr[s.charAt(slow)]++;
 
-                if (charsWithCount.containsKey(s.charAt(slow))) {
-                    int charCountSlow = charsWithCount.get(s.charAt(slow));
-                    if (charCountSlow == 0) currentDiffCharsCount++;
-                    charsWithCount.put(s.charAt(slow), ++charCountSlow);
-                }
-                slow++;
+                if (arr[s.charAt(slow++)] > 0) counter++;
             }
         }
-        return s.substring(start, end);
+        return min == Integer.MAX_VALUE ? "" : s.substring(startIndex, endIndex + 1);
     }
 }

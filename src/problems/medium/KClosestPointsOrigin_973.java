8fe9c173cbae15b54ccdb9307e4e956d07ad5d3b
bakhodir10 <bahodir9293@gmail.com>
2020-09-04 13:01:07 -0600

    kth point by sorting algorithm in leetcode

--- src/problems/medium/KClosestPointsOrigin_973.java
+++ src/problems/medium/KClosestPointsOrigin_973.java
@@ -5,8 +5,14 @@ import java.util.PriorityQueue;
 
 public class KClosestPointsOrigin_973 {
 
-    // Time complexity O(nlog(k)). Memory complexity O(k)
+    // Time complexity O(nlog(n)). Memory complexity O(n)
     public int[][] kClosest1(int[][] points, int k) {
+        Arrays.sort(points, (p1, p2) -> (p1[0] * p1[0] + p1[1] * p1[1]) - (p2[0] * p2[0] + p2[1] * p2[1]));
+        return Arrays.copyOfRange(points, 0, k);
+    }
+
+    // Time complexity O(nlog(k)). Memory complexity O(k)
+    public int[][] kClosest2(int[][] points, int k) {
         PriorityQueue<Pair> pQ = new PriorityQueue<>(k + 1);
         int[][] res = new int[k][2];
 
@@ -18,12 +24,6 @@ public class KClosestPointsOrigin_973 {
             res[--k] = new int[]{pQ.peek().x, pQ.poll().y};
         }
         return res;
-    }
-
-    // Time complexity O(nlog(n)). Memory complexity O(n)
-    public int[][] kClosest2(int[][] points, int k) {
-        Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] + p2[1] * p2[1]);
-        return Arrays.copyOfRange(points, 0, k);
     }
 
     private static class Pair implements Comparable<Pair> {

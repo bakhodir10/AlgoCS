bc585e423821a8df99485d1d4f05a9746d958f62
bakhodir10 <bahodir9293@gmail.com>
2020-09-06 04:33:24 -0600

    code optimization: kth smallest distance 719, kth smallest in matrix 378 leetcode

--- src/problems/hard/FindKThSmallest_719.java
+++ src/problems/hard/FindKThSmallest_719.java
@@ -4,14 +4,14 @@ public class FindKThSmallest_719 {
 
     public static int smallestDistancePair(int[] nums, int k) {
 
-        int arr[] = new int[1000001];
+        int[] arr = new int[1000001];
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) arr[Math.abs(nums[i] - nums[j])]++;
         }
 
         for (int i = 0; i < arr.length; i++) {
             k -= arr[i];
-            if (k == 0) return i;
+            if (k <= 0) return i;
         }
         return -1;
     }

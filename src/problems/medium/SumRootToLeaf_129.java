00046afe85f11d51ce1a348409c79a63871d2d05
bakhodir10 <bahodir9293@gmail.com>
2020-09-08 20:47:09 -0600

    leetcode sum of values from root to leaf problems solved

---
+++ src/problems/medium/SumRootToLeaf_129.java
@@ -0,0 +1,29 @@
+package problems.medium;
+
+public class SumRootToLeaf_129 {
+
+    private int sum = 0;
+
+    public int sumNumbers(TreeNode root) {
+        helper(root, 0);
+        return sum;
+    }
+
+    private void helper(TreeNode root, int num) {
+        if (root == null) return;
+
+        if (root.left == null && root.right == null) {
+            num = num * 10 + root.val;
+            sum += num;
+        }
+
+        helper(root.left, num * 10 + root.val);
+        helper(root.right, num * 10 + root.val);
+    }
+
+    private class TreeNode {
+        private int val;
+        private TreeNode left;
+        private TreeNode right;
+    }
+}

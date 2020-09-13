01ba0bae3b8d766a4ee2b150e073eb312a97ddc1
bakhodir10 <bahodir9293@gmail.com>
2020-09-12 23:28:57 -0600

    optimized find all anagram, solved replace all ? marks and make two strings anagrams on leetcode

--- src/problems/easy/FindAllAnagrams_438.java
+++ src/problems/easy/FindAllAnagrams_438.java
@@ -3,60 +3,54 @@ package problems.easy;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("Duplicates")
 public class FindAllAnagrams_438 {
 
-    public static List<Integer> findAnagrams(String word, String sub) {
+    // Time complexity: O(s.length + p.length). Space complexity: O(n)
+    public List<Integer> findAnagrams1(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
 
-        List<Integer> ans = new ArrayList<>();
+        int[] hash = new int[256];
+        int left = 0, count = p.length();
 
-        if (word.isEmpty()) return ans;
+        for (char c : p.toCharArray()) hash[c]++;
 
-        int hash = hashCode(word.charAt(0)), hashSub = hashCode(sub);
-        int lenSub = sub.length(), left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            if (hash[s.charAt(right)] > 0) count--;
+            hash[s.charAt(right)]--;
 
-        if (hash == hashSub) ans.add(left);
+            if (count == 0) list.add(left);
 
-        for (int i = 1; i < word.length(); i++) {
-
-            if (hash == hashSub && isAnagram(word.substring(left, i + 1), sub)) ans.add(left);
-
-            char newChar = word.charAt(i);
-            char oldChar = word.charAt(left);
-
-            if (i < lenSub) hash += hashCode(newChar);
-            else {
-                hash = changeHash(hash, oldChar, newChar);
+            if (right >= p.length() - 1) {
+                hash[s.charAt(left)]++;
+                if (hash[s.charAt(left)] > 0) count++;
                 left++;
             }
-            if (hash == hashSub && isAnagram(word.substring(left, i + 1), sub)) ans.add(left);
         }
-        return ans;
+        return list;
     }
 
-    private static int changeHash(int hash, char oldChar, char newChar) {
-        hash -= hashCode(oldChar);
-        hash += hashCode(newChar);
-        return hash;
+    // Time complexity: O(s.length + p.length). Space complexity: O(n)
+    public List<Integer> findAnagrams2(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+
+        int[] counts = new int[26];
+        int left = 0;
+
+        for (char c : p.toCharArray()) counts[c - 'a']++;
+
+        for (int right = 0; right < s.length(); right++) {
+            counts[s.charAt(right) - 'a']--;
+            if (isZero(counts)) list.add(left);
+            if (right >= p.length() - 1) counts[s.charAt(left++) - 'a']++;
+        }
+        return list;
     }
 
-    private static int hashCode(char c) {
-        return 31 * c;
-    }
-
-    private static int hashCode(String s) {
-        int hash = 0;
-        for (char c : s.toCharArray()) hash += hashCode(c);
-        return hash;
-    }
-
-    private static boolean isAnagram(String s, String p) {
-        if (s.length() != p.length()) return false;
-        int store[] = new int[26];
-        for (char c : s.toCharArray()) store[c - 'a']++;
-        for (char c : p.toCharArray()) store[c - 'a']--;
-        for (int i : store) {
-            if (i != 0) return false;
+    private boolean isZero(int[] counts) {
+        for (int count : counts) {
+            if (count != 0) return false;
         }
         return true;
     }

aff234548a5f277532432c6cb3fb2a25e5ba4e76
bakhodir10 <bahodir9293@gmail.com>
2020-09-20 14:04:05 -0600

    algorithm optimization on maximum non negative product using DP approach

--- src/problems/medium/MaximumNonNegativeProduct_5521.java
+++ src/problems/medium/MaximumNonNegativeProduct_5521.java
@@ -1,40 +1,42 @@
 package problems.medium;
 
-import java.util.Arrays;
-
 public class MaximumNonNegativeProduct_5521 {
 
-    // todo
-    // Approach. Dynamic Programming
-    public int maxProductPath1(int[][] grid) {
-        int[][] mins = new int[grid.length][grid[0].length];
+    // Approach => Dynamic Programming. Time complexity: O(n*m). Space complexity: O(n*m)
+    public int maxProductPath(int[][] grid) {
+        long[][] mins = new long[grid.length][grid[0].length];
+        long[][] maxes = new long[grid.length][grid[0].length];
         long mod = (long) 1e9 + 7;
+
         for (int i = 1; i < grid.length; i++) grid[i][0] *= grid[i - 1][0];
         for (int j = 1; j < grid[0].length; j++) grid[0][j] *= grid[0][j - 1];
 
-        for (int i = 0; i < grid.length; i++) mins[i][0] = grid[i][0];
-        for (int j = 0; j < grid[0].length; j++) mins[0][j] = grid[0][j];
+        for (int i = 0; i < grid.length; i++) {
+            mins[i][0] = grid[i][0];
+            maxes[i][0] = grid[i][0];
+        }
+        for (int j = 0; j < grid[0].length; j++) {
+            mins[0][j] = grid[0][j];
+            maxes[0][j] = grid[0][j];
+        }
 
         for (int i = 1; i < grid.length; i++) {
             for (int j = 1; j < grid[i].length; j++) {
-                mins[i][j] = Math.min(mins[i - 1][j] * grid[i][j], mins[i][j - 1] * grid[i][j]);
 
-                int max1 = Math.max(grid[i - 1][j] * grid[i][j], grid[i][j - 1] * grid[i][j]);
-                int max2 = Math.max(mins[i - 1][j] * grid[i][j], mins[i][j - 1] * grid[i][j]);
+                long min1 = Math.min(maxes[i - 1][j] * grid[i][j], maxes[i][j - 1] * grid[i][j]);
+                long min2 = Math.min(mins[i - 1][j] * grid[i][j], mins[i][j - 1] * grid[i][j]);
+                long max1 = Math.max(maxes[i - 1][j] * grid[i][j], maxes[i][j - 1] * grid[i][j]);
+                long max2 = Math.max(mins[i - 1][j] * grid[i][j], mins[i][j - 1] * grid[i][j]);
 
-                grid[i][j] = Math.max(max1, max2);
+                mins[i][j] = Math.min(min1, min2);
+                maxes[i][j] = Math.max(max1, max2);
             }
         }
-
-        for (int[] m : mins) System.out.println(Arrays.toString(m));
-        System.out.println("***************************************");
-        for (int[] g : grid) System.out.println(Arrays.toString(g));
-
-        return grid[grid.length - 1][grid[0].length - 1] < 0 ? -1
-                : (int) (grid[grid.length - 1][grid[0].length - 1] % mod);
+        return maxes[maxes.length - 1][maxes[0].length - 1] < 0 ? -1
+                : (int) (maxes[maxes.length - 1][maxes[0].length - 1] % mod);
     }
 
-    // Approach DFS.
+    // Approach => DFS.
     long max = Integer.MIN_VALUE;
     long mod = (long) 1e9 + 7;
 

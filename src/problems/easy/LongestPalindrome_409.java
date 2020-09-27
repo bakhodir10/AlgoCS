39d74acd8ec7c64863c64ed48eb69c641930270e
bakhodir10 <bahodir9293@gmail.com>
2020-09-27 00:41:51 -0600

    Leetcode weekly contest: CrawlerLogFolder_1598 and MaximumProfit_1599 solved;  LongestPalindrome_409 optimized, PalindromicSubstrings_647 solved

--- src/problems/easy/LongestPalindrome_409.java
+++ src/problems/easy/LongestPalindrome_409.java
@@ -1,22 +1,18 @@
 package problems.easy;
 
 public class LongestPalindrome_409 {
-    public static int longestPalindrome(String s) {
-        int arr[] = new int[60];
-        int ans = 0;
-        for (int i = 0; i < s.length(); i++) arr[s.charAt(i) - 'A']++;
 
-        boolean one = false;
-        for (int i = 0; i < arr.length; i++) {
-            if ((arr[i] == 1) && ((char) (i + 'A') >= 'A' && (char) (i + 'A') <= 'Z'
-                    || (char) (i + 'A') >= 'a' && (char) (i + 'A') <= 'z'))
-                one = true;
-            if (arr[i] % 2 == 0) ans += arr[i];
-            else {
-                one = true;
-                ans += arr[i] - 1;
-            }
+    public static int longestPalindrome(String s) {
+        int[] arr = new int[128];
+        int ans = 0;
+
+        for (char c : s.toCharArray()) arr[c]++;
+        boolean hasOdd = false;
+
+        for (int count : arr) {
+            ans += count / 2;
+            if (count % 2 == 1) hasOdd = true;
         }
-        return one ? ++ans : ans;
+        return hasOdd ? ans * 2 + 1 : ans * 2;
     }
 }

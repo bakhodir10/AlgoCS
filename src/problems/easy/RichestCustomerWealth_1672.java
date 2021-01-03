db6f1c0c1f293b5d936dab6fc8c38d3f665e6e3e
bakhodir10 <bahodir9293@gmail.com>
2021-01-03 17:51:33 -0600

    solved some easy level questions on leetcode

---
+++ src/problems/easy/RichestCustomerWealth_1672.java
@@ -0,0 +1,15 @@
+package problems.easy;
+
+public class RichestCustomerWealth_1672 {
+
+    // Time complexity: O(n). Space complexity: O(1);
+    public int maximumWealth(int[][] accounts) {
+        int res = 0;
+        for (int[] customerAcc : accounts) {
+            int sum = 0;
+            for (int acc : customerAcc) sum += acc;
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+}

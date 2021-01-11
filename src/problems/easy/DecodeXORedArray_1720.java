de864fa9e0406bc5279a6e25f6be191e873508d9
bakhodir10 <bahodir9293@gmail.com>
2021-01-11 14:30:11 -0600

    refactored decode xor array problem

--- src/problems/easy/DecodeXORedArray_1720.java
+++ src/problems/easy/DecodeXORedArray_1720.java
@@ -6,10 +6,7 @@ public class DecodeXORedArray_1720 {
     public int[] decode(int[] encoded, int first) {
         int[] newArr = new int[encoded.length + 1];
         newArr[0] = first;
-
-        for (int i = 1; i < newArr.length; i++) {
-            newArr[i] = encoded[i - 1] ^ newArr[i - 1];
-        }
+        for (int i = 1; i < newArr.length; i++) newArr[i] = encoded[i - 1] ^ newArr[i - 1];
         return newArr;
     }
 }

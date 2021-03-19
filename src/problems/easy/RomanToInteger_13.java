e436cf0b68cc7e3c7882d764849498f66bb12da6
bakhodir10 <bahodir9293@gmail.com>
2021-03-19 02:59:45 -0500

    solved integer to roman, roman to integer and check if string contains all binary with the size of k problems solved

--- src/problems/easy/RomanToInteger_13.java
+++ src/problems/easy/RomanToInteger_13.java
@@ -1,50 +1,39 @@
 package problems.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToInteger_13 {
-    public static int romainToInt(String s) {
-        if (s.isEmpty()) return 0;
 
-        int res = 0;
-        char last = 0;
+    // Time complexity: O(n). Space complexity: O(1)
+    public int romanToInt(String s) {
+        int i = 0;
+        int total = 0;
+        Map<String, Integer> map = new HashMap<>() {{
+            put("I", 1);
+            put("IV", 4);
+            put("V", 5);
+            put("IX", 9);
+            put("X", 10);
+            put("XL", 40);
+            put("L", 50);
+            put("XC", 90);
+            put("C", 100);
+            put("CD", 400);
+            put("D", 500);
+            put("CM", 900);
+            put("M", 1000);
+        }};
 
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-
-            if (current == 'I') {
-                last = 'I';
-                res += 1;
-            }
-            if (current == 'V') {
-                if (last == 'I') res += 3;
-                else res += 5;
-                last = 'V';
-            }
-            if (current == 'X') {
-                if (last == 'I') res += 8;
-                else res += 10;
-                last = 'X';
-            }
-            if (current == 'L') {
-                if (last == 'X') res += 30;
-                else res += 50;
-                last = 'L';
-            }
-            if (current == 'C') {
-                if (last == 'X') res += 80;
-                else res += 100;
-                last = 'C';
-            }
-            if (current == 'D') {
-                if (last == 'C') res += 300;
-                else res += 500;
-                last = 'D';
-            }
-            if (current == 'M') {
-                if (last == 'C') res += 800;
-                else res += 1000;
-                last = 'M';
+        while (i < s.length()) {
+            if (i + 2 <= s.length() && map.containsKey(s.substring(i, i + 2))) {
+                total += map.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                total += map.get(s.substring(i, i + 1));
+                i++;
             }
         }
-        return res;
+        return total;
     }
 }
